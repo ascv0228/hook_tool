270ad59a4a2422da51d5662e162eff1bb8d77723
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2021-05-24 21:28:21 +0800

    Create forkAndSpecializePre.cpp

---
+++ forkAndSpecializePre.cpp
@@ -0,0 +1,26 @@
+// forkAndSpecializePre
+// enable_hack = isGame(env, *appDataDir);
+int isGame(JNIEnv *env, jstring appDataDir) {
+    if (!appDataDir)
+        return 0;
+    const char *app_data_dir = env->GetStringUTFChars(appDataDir, nullptr);
+    int user = 0;
+    static char package_name[256];
+    if (sscanf(app_data_dir, "/data/%*[^/]/%d/%s", &user, package_name) != 2) {
+        if (sscanf(app_data_dir, "/data/%*[^/]/%s", package_name) != 1) {
+            package_name[0] = '\0';
+            LOGW("can't parse %s", app_data_dir);
+            return 0;
+        }
+    }
+    if (strcmp(package_name, "com.percent.royaldice") == 0) {
+        LOGI("detect game: %s", package_name);
+        game_data_dir = new char[strlen(app_data_dir) + 1];
+        strcpy(game_data_dir, app_data_dir);
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 1;
+    } else {
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 0;
+    }
+}
