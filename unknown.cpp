69686fb96fc6baaa23a331829834620c5de590f9
ascv0228 <77038091+ascv0228@users.noreply.github.com>
2021-05-24 21:28:59 +0800

    Create unknown.cpp

---
+++ unknown.cpp
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include "hook.h"
+#include <Logger.h>
+#include <pthread.h>
+// UserDBManager_AddTrophy
+
+Config::isbackgroundHack = true;
+    while (!il2cpp_handle) {
+        sleep(1);
+    }
+    while (!il2cpp_handle) {
+        sleep(1);
+    }
+    sleep(3);
+    
+// hack_thread
+// android_get_device_api_level
+// adb install --abi armeabi-v7a rd.apk
